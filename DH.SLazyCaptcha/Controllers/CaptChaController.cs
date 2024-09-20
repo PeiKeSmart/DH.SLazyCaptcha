f51e74380dc3054910a0a3c513727888851452b5
猿人易 <qcjxberin@outlook.com>
2024-09-20 19:47:15 +0800

    修复验证码注入

--- DH.SLazyCaptcha/Controllers/CaptChaController.cs
+++ DH.SLazyCaptcha/Controllers/CaptChaController.cs
@@ -9,12 +9,20 @@ namespace DH.SLazyCaptcha.Controllers;
 /// </summary>
 [ApiExplorerSettings(IgnoreApi = true)]
 public partial class CaptChaController : Controller {
+
+    public readonly ICaptcha Captcha;
+
+    public CaptChaController(ICaptcha _Captcha)
+    {
+        Captcha = _Captcha;
+    }
+
     /// <summary>
     /// 验证码，适用于跨平台。
     /// </summary>
     /// <returns></returns>
     [RateValve(Policy = Policy.Ip, Limit = 30, Duration = 60)]
-    public IActionResult GetCheckCode(ICaptcha Captcha)
+    public IActionResult GetCheckCode()
     {
         var info = Captcha.Generate();
         var stream = new MemoryStream(info.Bytes);
